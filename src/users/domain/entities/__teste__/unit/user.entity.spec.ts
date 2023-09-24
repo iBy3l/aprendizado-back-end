c6bc5d4b152383e14c4007cc5e802513afe7fa5d
iBy3l <looqgabriel@gmail.com>
2023-09-24 00:37:43 -0300

    create getter in userentity e test

--- src/users/domain/entities/__teste__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__teste__/unit/user.entity.spec.ts
@@ -20,4 +20,27 @@ describe('UserEntity unit test', () => {
     expect(sut.props.password).toEqual(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
+
+  it('name getter', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('email getter', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('password getter', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('createdAt getter', () => {
+    expect(sut.props.createdAt).toBeDefined()
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
 })
