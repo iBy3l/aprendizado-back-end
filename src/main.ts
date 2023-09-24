d749206407608cd29bb6f247b1472601733c20a4
iBy3l <looqgabriel@gmail.com>
2023-09-23 21:27:51 -0300

    config api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  console.log('http://localhost:3000');
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
