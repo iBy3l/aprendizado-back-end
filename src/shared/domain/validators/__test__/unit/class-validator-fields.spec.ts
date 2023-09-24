c4a4f9887141f9096b6b8f78038f5cab2fa78ad2
iBy3l <looqgabriel@gmail.com>
2023-09-24 13:16:34 -0300

    test unit class validate

---
+++ src/shared/domain/validators/__test__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,28 @@
+import * as libClassValidator from 'class-validator'
+import { ClassValidatorFields } from '../../class-validator-fields'
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field1: string
+}> {}
+
+describe('ClassValidatorFields unit tests', () => {
+  it('should return true if validation with null', () => {
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.errors).toBeNull()
+    expect(sut.validateData).toBeNull()
+  })
+
+  it('should validate with errors ', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([
+      { property: 'field1', constraints: { isRequired: 'Test error' } },
+    ])
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate(null)).toBeFalsy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validateData).toBeNull()
+    expect(sut.errors).toStrictEqual({ field1: ['Test error'] })
+  })
+})
